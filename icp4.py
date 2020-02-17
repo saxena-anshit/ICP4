a88ce643e9bb435cf3bc2e3af865ab7b3eb6e731
ansh <saxena.anshit@gmail.com>
2020-02-18 01:01:36 +0530

    ss added 2

--- icp4.py
+++ icp4.py
@@ -12,15 +12,15 @@ sc = SparkContext.getOrCreate()
 #     .reduceByKey(lambda a, b: a + b)
 # print(x.collect())
 
-# x = sc.textFile("/home/ansh/PycharmProjects/ICP4/data.csv") \
-#      .map(lambda line: line.split(",")) \
-#      .map(lambda line: (line[7])) \
-#      .distinct()
-# print(x.collect())
-# print(x.count())
-# print(x.take(2))
-
 x = sc.textFile("/home/ansh/PycharmProjects/ICP4/data.csv") \
-    .map(lambda line: line.split(","))
-df4 = x.filter(lambda line: "105.5" in line).collect()
-print(df4)
+     .map(lambda line: line.split(",")) \
+     .map(lambda line: (line[7])) \
+     .distinct()
+print(x.collect())
+print(x.count())
+print(x.take(2))
+#
+# x = sc.textFile("/home/ansh/PycharmProjects/ICP4/data.csv") \
+#     .map(lambda line: line.split(","))
+# df4 = x.filter(lambda line: "105.5" in line).collect()
+# print(df4)
